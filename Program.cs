c049f2a0e504ea020ebc3fb5904fa7db72312736
Samiul Haque Atul <samhaque999@gmail.com>
2024-02-27 05:18:19 +0600

    ASP DOt Net Core Web Application setup.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(); // Added this line to register controller services
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline ( Sequence is important)
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
