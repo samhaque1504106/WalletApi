23037984cad3951155f0c1488c99bbff752e4fb9
Samiul Haque Atul <samhaque999@gmail.com>
2024-03-07 02:10:52 +0600

    #[Index(nameof(UserName),IsUnique = true)] // this is done through EF core. but also can be done using fluent api. #[JsonIgnore] to avoid CreatedAt and UpdatedAt datetime fields in request payload. #regex for email validation. #added required fields.

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,8 +1,11 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
 
 namespace WalletApi.Models
 {
+    [Index(nameof(UserName),IsUnique = true)] // this is done through EF core. but also can be done using fluent api.
     public class Users
     {
         [Key]
@@ -10,6 +13,7 @@ namespace WalletApi.Models
         public long Id { get; set; }
 
         [Required(ErrorMessage = "UserName is Must!")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Use at least 5 characters(Use symbols for unique UserName)")] 
         public string? UserName { get; set; }
 
         [Required(ErrorMessage = "Password is Must!")]
@@ -19,11 +23,14 @@ namespace WalletApi.Models
         [Required(ErrorMessage = "Email is Must!")]
         [RegularExpression(@"^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$", ErrorMessage = "Invalid email format.")]
         [EmailAddress]
+        [StringLength(500)]
         public string? Email { get; set; }
 
         public string? UserStatus { get; set; }
-
+        
+        [JsonIgnore] //to avoid in request payload
         public DateTimeOffset? CreatedAt { get; set; }
+        [JsonIgnore]
         public DateTimeOffset? UpdatedAt { get; set; }
 
         public string? SignInStatus { get; set; }
