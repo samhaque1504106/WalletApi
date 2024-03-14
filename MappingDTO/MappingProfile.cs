5b4614f3efee37a5975700336d0d0b5eba4e93d1
Samiul Haque Atul <samhaque999@gmail.com>
2024-03-14 16:21:39 +0600

    MappingProfile that creates Map: CreateMap<Users, UsersDTO>().ReverseMap();

---
+++ MappingDTO/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using WalletApi.Models;
+
+namespace WalletApi.MappingDTO
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        { 
+            CreateMap<Users, UsersDTO>().ReverseMap();
+        }
+    }
+}
