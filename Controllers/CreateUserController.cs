444ea9cc21a4f163ab7223e297170b94395c9167
Samiul Haque Atul <samhaque999@gmail.com>
2024-03-14 16:23:09 +0600

    brought changes in from the first CreateUser endpoint. in the request payload CreatedAt and UpdatedAt fields as avoided, but for saving in the db CreatedAt takes current UTC time and UpdatedAt is null. Automapper is used.

--- Controllers/CreateUserController.cs
+++ Controllers/CreateUserController.cs
@@ -6,6 +6,7 @@ using Microsoft.Data.SqlClient;
 using System.Data;
 using System.Collections;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using AutoMapper;
 
 namespace WalletApi.Controllers
 {
@@ -15,37 +16,39 @@ namespace WalletApi.Controllers
     {
         private readonly ApplicationDbContext _db;
         public ILogger<CreateUserController> _logger;
-        public CreateUserController(ILogger<CreateUserController> logger, ApplicationDbContext db)
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+        public CreateUserController(ILogger<CreateUserController> logger, ApplicationDbContext db, IConfiguration configuration, IMapper mapper)
         {
             _logger = logger;
-            _db = db;   
-        }
-        [HttpGet()]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public string GetUser()
-        {
-            Console.WriteLine("Inside GET request in hello");
-            return ("hello");
+            _db = db;
+            _configuration = configuration;
+            _mapper = mapper;
         }
         
+
+
+
         //create user
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
-        public ActionResult<Users> CreateUser([FromBody] Users user)
+        public ActionResult<Users> CreateUser([FromBody] UsersDTO userdto)
         {
+            if (userdto == null)
+            {
+                return BadRequest(userdto);
+            }
+            var user = _mapper.Map<Users>(userdto);
+
             if (_db.Users.FirstOrDefault(u => u.UserName.ToLower() == user.UserName.ToLower()) != null)
             {
-                ModelState.AddModelError("CustomError","UserName already taken,try using combinations with number and signs!");
+                ModelState.AddModelError("CustomError", "UserName already taken,try using combinations with number and signs!");
                 return Conflict(ModelState);
             }
-            else if (user == null)
-            {
-                return BadRequest(user);
-            }
+             
             else if (user.Id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
@@ -54,12 +57,15 @@ namespace WalletApi.Controllers
             // Set CreatedAt to current UTC time and UpdatedAt to null
             user.CreatedAt = DateTime.UtcNow;
             user.UpdatedAt = null;
-            
+
+
             _db.Users.Add(user);
             _db.SaveChanges();
-            _logger.LogInformation("new sign up username: "+user.UserName+" is done!");
+            _logger.LogInformation("new sign up username: " + user.UserName + " is done!");
             return Ok("sign-up successful, welcome to WalletApi!");
         }
+
+
     }
 }
 
